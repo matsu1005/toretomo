3ddbc840e0238b09177a00f3f9fbcae73bf63fd0
matsu <forte4262@gmail.com>
2021-08-04 00:58:19 +0900

    ログイン、ログアウト時のvalidatoin errorの表示

---
+++ front/store/errorMessage.js
@@ -0,0 +1,26 @@
+export const state = () => ({
+  messages: ''
+})
+
+export const getters = {
+  getMessages: (state) => state.messages,
+  existsMessages: (state) => state.messages !== ''
+}
+
+export const mutations = {
+  setMessages(state, messages) {
+    state.messages = messages
+  },
+  clearMessages(state) {
+    state.messages = ''
+  },
+}
+
+export const actions = {
+  setMessages( { commit }, messages) {
+    commit('setMessages', messages)
+  },
+  clearMessages( { commit } ) {
+    commit('clearMessages')
+  }
+}
