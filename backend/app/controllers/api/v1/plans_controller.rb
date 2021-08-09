167e8cfc1c0943dd81371935d2d053dbf24e87a8
matsu <forte4262@gmail.com>
2021-08-09 09:31:00 +0900

    plan一覧表示、投稿機能の実装

--- backend/app/controllers/api/v1/plans_controller.rb
+++ backend/app/controllers/api/v1/plans_controller.rb
@@ -3,10 +3,25 @@ module Api
     class PlansController < ApplicationController
 
       def index
-        @plan = Plan.all.includes(:user)
-        render json: @plan.as_json(includes: [:user])
+        @plans = Plan.all.includes(:user)
+        render json: @plans.as_json(includes: [:user])
       end
       
+      def create
+        @plan = Plan.new(plan_params)
+        if @plan.save
+          render json: @plan
+        else 
+          render status: 400, json: {errors: {status: 400, error: @plan.errors.full_messages}}
+        end
+      end
+
+      private
+
+      def plan_params
+        params.permit(:user_id, :title, :detail, :start_ymd, :start_time, :duration, :event_cls, :train_strength, :place, :prefecture, :join_limit)
+      end
+
     end
   end    
 end
--- backend/app/models/plan.rb
+++ backend/app/models/plan.rb
@@ -1,4 +1,16 @@
 class Plan < ApplicationRecord
 
   belongs_to :user
+
+  validates :title, presence: true, length: {maximum: 75}
+  validates :detail, presence: true
+  validates :start_ymd, presence: true
+  validates :start_time, presence: true
+  validates :duration, presence: true
+  validates :event_cls, presence: true
+  validates :train_strength, presence: true
+  validates :place, presence: true, length: {maximum: 50}
+  validates :prefecture, presence: true
+  validates :join_limit, presence: true, numericality: {less_than: 20}
+
 end
