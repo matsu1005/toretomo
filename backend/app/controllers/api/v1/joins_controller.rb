4010356a763e9e04071eb4ca3294816c3f361a72
matsu <forte4262@gmail.com>
2021-08-20 02:07:11 +0900

    コントローラーの修正 #7

--- backend/app/controllers/api/v1/joins_controller.rb
+++ backend/app/controllers/api/v1/joins_controller.rb
@@ -1,6 +1,31 @@
 module Api
   module V1
     class JoinsController < ApplicationController
+
+      def create
+        join = Join.new(join_params)
+        if join.save
+          render json: join
+        else
+          render status: 400, json: {errors: {status: 400, error: join.errors.full_messages}}
+        end
+      end
+
+      def destroy
+        join = Join.find(params[:id])
+        if join.destroy
+          render json: @user
+        else
+          render status: 400, json: {errors: {status: 400, error: join.errors.full_messages}}
+        end
+      end
+
+      private
+
+      def join_params
+        params.permit(:user_id, :plan_id)
+      end
+
     end
   end
 end
--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -3,8 +3,30 @@ module Api
     class UsersController < ApplicationController
       
       def show
-        @user = User.includes({plan: :user}).find(params[:id])
-        render json: @user.as_json(include: [{plan: {include: {user: {only: %w[id name icon]}}}}])
+        @user = User.includes({plan: [:user, :joins ]}, {planjoin: [:join_users, :user ]}).find(params[:id])
+        render json: @user.as_json(include: [{
+            plan: {
+              include: [{
+                user: {
+                  only: %w[id name icon]
+                },
+                joins: {
+                  only: %w[id user_id]
+                },
+              }]
+            }, 
+            planjoin: {
+              include: [{
+                joins: {
+                  only: %w[id user_id]
+                },
+                user: {
+                  only: %w[id name icon]
+                }
+              }]
+            }
+          }
+          ])
       end
 
     end
